68577e24e552770c0ee185d49c9503f25935c430
PauRaset <94690926+PauRaset@users.noreply.github.com>
2025-10-05 13:03:33 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,16 @@
-// app/page.jsx
+/*// app/page.jsx
 import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Home = listado de tus eventos
   redirect('/(app)/events');
-}
+}*/
 
-
-/*// app/page.jsx
+// app/page.jsx
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // Redirige la home del panel al login
-  redirect('/login');
+  // Elige UNA de estas y deja la otra comentada
+  redirect('/events');     // ← si tu Home es el listado de eventos
+  // redirect('/dashboard'); // ← si prefieres llevarlo al dashboard
 }
-*/
